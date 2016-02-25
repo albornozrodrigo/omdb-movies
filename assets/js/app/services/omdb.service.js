0889fc203f8390383b92f6df4f17cb72ebd106b7
Rodrigo Albornoz <rodrigo.albornoz.f@gmail.com>
2016-02-24 23:08:44 -0300

    Added https to endpoint

--- assets/js/app/services/omdb.service.js
+++ assets/js/app/services/omdb.service.js
@@ -9,7 +9,7 @@
 	omdbService.$inject = ['$resource'];
 
 	function omdbService($resource) {
-		var url = 'http://www.omdbapi.com';
+		var url = 'https://www.omdbapi.com';
 		var params = {
 			title: '@title',
 			page: '@page',
@@ -20,10 +20,6 @@
 			search: {
 				method: 'GET',
 				url: url + '/?s=:title&type=movie&page=:page',
-				/*isArray: true,
-				transformResponse: function(data) {
-					return JSON.parse(data).Search;
-				}*/
 			},
 			info: {
 				method: 'GET',
